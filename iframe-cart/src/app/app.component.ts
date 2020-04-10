918778ddfeb281a898cb7f00ec3af2ac6e1f1de4
Engedi <emese.engedi@accenture.com>
2020-04-10 12:54:45 +0200

    :sparkles: items can be removed from cart

--- iframe-cart/src/app/app.component.ts
+++ iframe-cart/src/app/app.component.ts
@@ -1,21 +1,34 @@
 import { Component, HostListener } from '@angular/core';
+import { Item } from './item';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  items: Item[] = [];
+
   @HostListener('window:message', ['$event'])
   onMessage(event) {
     if (event.origin.startsWith('http://localhost:3000')) {
-      this.title = event.data;
-      console.log(JSON.stringify(event.data));
+      this.items.push(
+        new Item(
+          event.data.id,
+          event.data.name,
+          event.data.description,
+          event.data.price
+        )
+      );
     }
   }
+
   constructor() {
     console.log('bar');
   }
 
-  title = 'iframe-cart';
+  removeItem(item) {
+    this.items = this.items.filter((it) => it.id != item.id);
+    window.parent.postMessage(item.id, 'http://localhost:3000');
+  }
 }
--- iframe-container/src/App.js
+++ iframe-container/src/App.js
@@ -5,45 +5,62 @@ import Navigation from "./components/Navigation"
 
 function App() {
   const [products, setProducts] = useState([])
+  const [cartToggle, setCartToggle] = useState(false)
   useEffect(() => {
-    const sendReceived = (event) => {
+    const sendProductToCart = (event) => {
       if (event.origin.startsWith("http://localhost:3001")) {
         setProducts((products) => [...products, event.data.name])
-        document
-          .querySelectorAll("iframe")
-          .forEach((iframe) =>
-            iframe.contentWindow.postMessage(event.data, "*")
-          )
+        const pi = document.getElementById("products-iframe")
+        pi.contentWindow.postMessage(event.data, "http://localhost:3002")
+      }
+    }
+    const sendItemToProducts = (event) => {
+      if (event.origin.startsWith("http://localhost:3002")) {
+        setProducts(products.filter((p) => p.id != event.data))
+        const ci = document.getElementById("cart-iframe")
+        ci.contentWindow.postMessage(event.data, "http://localhost:3001")
       }
     }
 
-    window.addEventListener("message", sendReceived)
-    return () => window.removeEventListener("message", sendReceived)
+    window.addEventListener("message", sendProductToCart)
+    window.addEventListener("message", sendItemToProducts)
+    return () => {
+      window.removeEventListener("message", sendProductToCart)
+      window.removeEventListener("message", sendItemToProducts)
+    }
   }, [])
+
+  const handleCartToggle = (cartState) => {
+    setCartToggle(cartState)
+  }
 
   return (
     <Router>
       <div className="App">
-        <Navigation products={products} />
-        <Route exact path="/">
+        <Navigation
+          products={products}
+          cartToggle={cartToggle}
+          onCartToggleChange={handleCartToggle}
+        />
+        <section className="h-full flex relative pt-24">
           <iframe
             src="http://localhost:3001/product/"
             frameBorder="0"
+            scrolling="no"
             title="Products made with Vue"
             width="100%"
-            height="100%"
-            id="cart"
+            id="cart-iframe"
           ></iframe>
+
           <iframe
             src="http://localhost:3002"
             frameBorder="0"
+            scrolling="no"
             title="Cart made with Angular"
-            width="100%"
-            height="100%"
-            id="products"
+            id="products-iframe"
+            className={cartToggle ? "fixed w-full" : "hidden"}
           ></iframe>
-        </Route>
-        <Route path="/cart"></Route>
+        </section>
       </div>
     </Router>
   )
